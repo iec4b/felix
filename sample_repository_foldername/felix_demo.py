254ecaf37aa5642de25131d8a5caa5125a74e05b
iec4b <78461099+iec4b@users.noreply.github.com>
2023-12-27 06:03:27 -0500

    Add essential files

---
+++ sample_repository_foldername/felix_demo.py
@@ -0,0 +1,113 @@
+# felix.py
+
+# Import modules.
+# "fido" is an external python file that handles retrieval of metadata from a
+# prepared csv file.
+# this program requires the "pyperclip" module. See https://pypi.python.org/pypi/pyperclip
+
+import sys
+import pyperclip
+import fido_demo
+import subprocess
+
+
+# Set up for system options and arguments. 
+# I copied this basic parser code and the if/elif statements structure from
+# https://realpython.com/python-command-line-arguments/#a-few-methods-for-parsing-python-command-line-arguments
+
+opts = [opt for opt in sys.argv[1:] if opt.startswith("-")]
+args = [arg for arg in sys.argv[1:] if not arg.startswith("-")]
+
+# If your unique identifier or call number adheres to a standard format, you
+# can use the following optional variable to simplify the input so that only
+# the unique element of the identifier need be entered as the argument. For
+# example, if your call numbers use a format such as, "Box ##:##", replace
+# [your string] with "Box", and then you will only need to enter the numerical
+# component of the identifier, without "Box" each time. This saves time and
+# typing.  unique_id =  "[your string] " + " ".join(arg for arg in args)
+# Currently, the unique_id is set up to accept arguments as they are entered.
+
+unique_id  = " ".join(arg for arg in args)
+print(unique_id)
+
+# Run the fido.search function on the unique_id. See the file fido.py. 
+
+metadata = fido_demo.search(unique_id)
+
+# Display the duration information as a MARC 300 field, and copy it to the
+# system's clipboard.
+
+if "-d" in opts:
+    print(metadata['MARC300'])
+    pyperclip.copy(metadata['MARC300'])
+
+# Display the size information as a MARC 387 field, and copy it to the system's
+# clipboard.
+
+elif "-s" in opts:
+    print(metadata['MARC387'])
+    pyperclip.copy(metadata['MARC387'])
+
+# Display the processing notes as a MARC 500 field, and copy it to the system's
+# clipboard.
+
+elif "-p" in opts:
+    print(metadata['ProcessingNotes'])
+    pyperclip.copy(metadata['ProcessingNotes'])
+
+# Display the citation information as a MARC 524 field, and copy it to the
+# system's clipboard.
+
+elif "-c" in opts:
+     print(metadata['MARC524'])
+     pyperclip.copy(metadata['MARC524'])
+
+# Display the location information as a MARC 852 field, and copy it to the
+# system's clipboard.
+
+elif "-l" in opts:
+     print(metadata['MARC852'])
+     pyperclip.copy(metadata['MARC852'])
+
+# Open images associated with the items using the default viewer. The current
+# configuration opens only jpeg files ending in ".JPG", but this can be
+# adjusted as desired by changing the subprocess.call command below.  
+
+elif "-i" in opts:
+	print(f"Opening images from {metadata['FolderPath']}.")
+	subprocess.call(['open *.JPG'], cwd=metadata['FolderPath'], shell='TRUE')
+
+# Open audio associated with the items using the default player. The current
+# configuration opens only mp3 files ending in ".mp3", but this can be adjusted
+# as desired by changing the subprocess.call command below.  
+
+elif "-a" in opts:
+	print(f"Opening audio from {metadata['FolderPath']}.")
+	subprocess.call(['open *.mp3'], cwd=metadata['FolderPath'], shell='TRUE')
+
+# Open audio associated with the items using vlc CLI. The current
+# configuaration opens only mp3 files ending in ".mp3", but this can be
+# adjusted as desired by changing the subprocess.call command below. Requires
+# VLC CLI. The "--no-media-library" option avoids opening the VLC CLI with
+# other files already queued. 
+
+elif "-ak" in opts:
+	print(f"Opening audio from {metadata['FolderPath']}.")
+	subprocess.call(['vlc --no-media-library *mp3'], cwd=metadata['FolderPath'], shell='TRUE')
+
+# Display the path to the items with the entered unique id, and copy it to the
+# system's clipboard.
+
+elif "-f" in opts:
+	print(metadata['FolderPath'])
+	pyperclip.copy(metadata['FolderPath'])
+
+# Display felix.py usage instructions.
+
+elif "-h" in opts:
+    raise SystemExit(f"Usage: felix [-opt] [Your unique identifier]\n\t-d <display and copy to clipboard MARC300>\n\t-s <display and copy to clipboard MARC387>\n\t-p <display and copy to clipboard Processing Notes>\n\t-c <display and copy to clipboard MARC524>\n\t-l <display and copy to clipboard MARC852>\n\t-i <Open images in default viewer>\n\t-a <Play audio in default media player>\n\t-ak <Play audio in console, requires VLC CLI>\n\t-h <display this instructional message>")
+
+# Displays the same usage instructions as for "-h", if anything invalid is entered as an option.
+
+else: 
+    raise SystemExit(f"Usage: felix [-opt] [Your unique identifier]\n\t-d <display and copy to clipboard MARC300>\n\t-s <display and copy to clipboard MARC387>\n\t-p <display and copy to clipboard Processing Notes>\n\t-c <display and copy to clipboard MARC524>\n\t-l <display and copy to clipboard MARC852>\n\t-i <Open images in default viewer>\n\t-a <Play audio in default media player>\n\t-ak <Play audio in console, requires VLC CLI>\n\t-h <display this instructional message>")
---
+++ sample_repository_foldername/fido_demo.py
@@ -0,0 +1,81 @@
+# fido.py
+
+import csv
+
+# Define a function to return values based on the unique_id entered with felix.py.
+
+def search(unique_id):
+
+# Set up empty lists for duration and size values to be populated later.
+
+	duration_field_value = []
+	size_field_value = []
+
+# Run the search function using the prepared csv file, data_warehouse.csv. 
+
+	with open('data_warehouse.csv', mode='r', encoding='utf-8-sig') as f:
+		file_reader = csv.DictReader(f, delimiter=',')
+
+    # Match values by comparing unique_id to the value in FullCallNumber.
+
+		for row in file_reader:
+			if row['FullCallNumber'] == unique_id:
+				minutes = row['durationMinutes']
+				seconds = row['durationSeconds']
+
+        # The manipulations below alter duration and size formats to suit the MARC fields. Ideally, they should adhere to ISBD standards.
+        
+				fixed_seconds = seconds.zfill(2)
+				duration = minutes+":"+fixed_seconds
+				size = float(row['filesizeKB'])
+				just_size = str(format(size, '.2f'))
+
+        # Populate the empty lists defined above.
+
+				duration_field_value.append(duration)
+				size_field_value.append(just_size)
+
+        # Define variables to be used below, in the fido.search() return array.
+        
+				processing_notes = "500  "+row['ProcessingNotes']
+				field_524 = f"524 [your citation string] {unique_id}."
+				field_852 = f"8528 [Location] ǂb [Sublocation or collection] ǂc [Shelving location] ǂh {unique_id} ǂx [nonpublic note, e.g. cataloger's initials]"
+				folder_path = row['FolderPath']
+
+# The following conditional applies if there is more than one file associated with the digitized audio, i.e. for a two-sided cassette tape.
+
+	if len(duration_field_value)>1:
+		field_300="300  2 mp3 files ("+duration_field_value[0]+" min., "+duration_field_value[1]+" min.) : ǂb sound"
+		field_387="387  audio file ǂb MP3 ǂc "+size_field_value[0]+", "+size_field_value[1]+" KB ǂ2 rda"
+
+    # Define the return array for a two-sided cassette tape.
+
+		return(
+			{
+			'MARC300' : field_300, 
+			'MARC387' : field_387, 
+			'ProcessingNotes':processing_notes,
+			'FolderPath' : './'+folder_path,
+			'MARC524' : field_524,
+			'MARC852' : field_852
+			}
+			)
+
+# The following conditional applies if there is a single file associated with the digitized audio, i.e. for a cassette tape where one side is blank.
+
+	if len(duration_field_value)==1:
+		field_300="300  1 mp3 file ("+duration_field_value[0]+" min.) : ǂb sound"
+		field_387="387  audio file ǂb MP3 ǂc "+size_field_value[0]+" KB ǂ2 rda"
+
+    # Define the return array for a "one-sided" cassette tape.
+
+		return(
+			{
+			'MARC300' : field_300, 
+			'MARC387' : field_387, 
+			'ProcessingNotes':processing_notes,
+			'FolderPath' : '/Users/micahlanger/Library/CloudStorage/OneDrive-JewishTheologicalSeminaryofAmerica/NHPRC/'+folder_path,
+			'MARC524' : field_524,
+			'MARC852' : field_852
+			}
+			)
